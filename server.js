19810489eada92e3303d49e2ae5018db4b073bb0
Ádám Berki <adamberki97@gmail.com>
2019-01-20 12:53:31 +0100

    fix heroku error 4

--- server.js
+++ server.js
@@ -5,11 +5,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/opkutpeldatar'));
+app.use(express.static(__dirname + '/dist/opkut-peldatar'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname+'/dist/opkutpeldatar/index.html'));
+res.sendFile(path.join(__dirname+'/dist/opkut-peldatar/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
