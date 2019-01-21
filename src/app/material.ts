e9b889d2b5a36875726b7a27d4229b039fd7d1ff
Ádám Berki <adamberki97@gmail.com>
2019-01-21 22:27:19 +0100

    add paper effect

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,10 +1,10 @@
-import {MatButtonModule, MatCheckboxModule} from '@angular/material';
+import {MatButtonModule, MatCheckboxModule, MatDividerModule, MatListModule} from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import { NgModule } from '@angular/core';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
-  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatDividerModule, MatListModule],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatDividerModule, MatListModule],
 })
 export class MaterialModule { }
