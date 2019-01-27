72dbb8887d9d00056482f6f8301a644d01299efd
Ádám Berki <adamberki97@gmail.com>
2019-01-27 18:37:04 +0100

    add isCodesSelected[] boolean List for keep in mind the selected languages that will be downloaded

--- src/app/chapter1/chapter1.component.ts
+++ src/app/chapter1/chapter1.component.ts
@@ -9,7 +9,7 @@ export class Chapter1Component implements OnInit {
 
   codes: string[] = ['R', 'Lingo', 'SAS/OR'];
   current_selected: string;
-  selectedItemValue: any;
+  isCodesSelected: boolean[] = [false, false, false];
 
   constructor() { }
 
@@ -17,12 +17,31 @@ export class Chapter1Component implements OnInit {
   }
 
   onSelection(e, v) {
-    console.error(e.option.selected,v); 
+    console.log(e.option.selected,v); 
     this.current_selected = e.option.value;
     console.log(this.current_selected + " is choosed");
+
+    switch(this.current_selected) {
+      case this.codes[0]: {
+        this.isCodesSelected[0] = !this.isCodesSelected[0];
+        break;
+      }
+      case this.codes[1]: {
+        this.isCodesSelected[1] = !this.isCodesSelected[1];
+        break;
+      }
+      case this.codes[2]: {
+        this.isCodesSelected[2] = !this.isCodesSelected[2];
+        break;
+      }
+    }
+
   }
 
   downloadCodes() {
-    console.log("");
+    console.log(this.isCodesSelected);
+    console.log("R is: " + this.isCodesSelected[0]);
+    console.log("Lingo is: " + this.isCodesSelected[1]);
+    console.log("SAS/OR is: " + this.isCodesSelected[2]);
   }
 }
