721a78edd4d2c9e21e943d05387972bbc8e39f13
Ádám Berki <adamberki97@gmail.com>
2019-01-31 23:16:53 +0100

    Finish copiing to chapter1 tags

--- src/app/chapter1/chapter1.component.ts
+++ src/app/chapter1/chapter1.component.ts
@@ -12,17 +12,166 @@ export class Chapter1Component implements OnInit {
   ngOnInit() {
   }
 
+  //R codes
   downloadRcode(){
     window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_01.R");
-    console.log("file downloaded");
+    window.location.href='../../assets/sources/files/kodok/r/1 fejezet feladatai/I_01(intpoint package).R';
+    console.log("files successfully downloaded");
   }
 
+  downloadRcode2(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_02.R");
+    console.log("files successfully downloaded");
+  }
+
+  downloadRcode3(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_03.R");
+    console.log("files successfully downloaded");
+  }
+
+  downloadRcode4(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_04.R");
+    console.log("files successfully downloaded");
+  }
+
+  downloadRcode5(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_05.R");
+    console.log("files successfully downloaded");
+  }
+
+  downloadRcode6(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_06.R");
+    console.log("files successfully downloaded");
+  }
+
+  downloadRcode7(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_07.R");
+    console.log("files successfully downloaded");
+  }
+
+  downloadRcode8(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_08.R");
+    console.log("files successfully downloaded");
+  }
+
+  downloadRcode9(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_09.R");
+    console.log("files successfully downloaded");
+  }
+
+  downloadRcode10(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_10.R");
+    console.log("files successfully downloaded");
+  }
+
+  downloadRcode11(){
+    window.open("../../assets/sources/files/kodok/r/1 fejezet feladatai/I_11.R");
+    console.log("files successfully downloaded");
+  }
+
+  //Lingo codes
   downloadLingocode(){
     window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_01.lg4");
-    console.log("file downloaded");
+    console.log("file successfully downloaded");
   }
 
-  downloadSascode(){
+  downloadLingocode2(){
+    window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_02.lg4");
+    console.log("file successfully downloaded");
+  }
+
+  downloadLingocode3(){
+    window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_03.lg4");
+    console.log("file successfully downloaded");
+  }
+
+  downloadLingocode4(){
+    window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_04.lg4");
+    console.log("file successfully downloaded");
+  }
+
+  downloadLingocode5(){
+    window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_05.lg4");
+    console.log("file successfully downloaded");
+  }
+
+  downloadLingocode6(){
+    window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_06.lg4");
+    console.log("file successfully downloaded");
+  }
+
+  downloadLingocode7(){
+    window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_07.lg4");
+    console.log("file successfully downloaded");
+  }
+
+  downloadLingocode8(){
+    window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_08.lg4");
+    console.log("file successfully downloaded");
+  }
+
+  downloadLingocode9(){
+    window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_09.lg4");
+    console.log("file successfully downloaded");
+  }
+
+  downloadLingocode10(){
+    window.open("../../assets/sources/files/kodok/Lingo/1 fejezet feladatai/I_10.lg4");
+    console.log("file successfully downloaded");
+  }
+
+  downloadLingocode11(){
+    console.log("no file found");
+    //window.open("");
+  }
+
+  //SAS/ OR codes
+  downloadSascode2(){
+    window.open("../../assets/sources/files/kodok/sas or/1 fejezet feladatai/G.2.sas");
+    console.log("file successfully downloaded");
+  }
+
+  downloadSascode3(){
+    window.open("../../assets/sources/files/kodok/sas or/1 fejezet feladatai/G.3.sas");
+    console.log("file successfully downloaded");
+  }
+
+  downloadSascode4(){
+    window.open("../../assets/sources/files/kodok/sas or/1 fejezet feladatai/G.4.sas");
+    console.log("file successfully downloaded");
+  }
+
+  downloadSascode5(){
+    window.open("../../assets/sources/files/kodok/sas or/1 fejezet feladatai/G.5.sas");
+    console.log("file successfully downloaded");
+  }
+
+  downloadSascode6(){
+    window.open("../../assets/sources/files/kodok/sas or/1 fejezet feladatai/G.6.sas");
+    console.log("file successfully downloaded");
+  }
+
+  downloadSascode7(){
+    console.log("no file found");
+    //window.open("");
+  }
+
+  downloadSascode8(){
+    console.log("no file found");
+    //window.open("");
+  }
+
+  downloadSascode9(){
+    console.log("no file found");
+    //window.open("");
+  }
+
+  downloadSascode10(){
+    console.log("no file found");
+    //window.open("");
+  }
+
+  downloadSascode11(){
     console.log("no file found");
     //window.open("");
   }
