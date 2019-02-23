55962d5f8859ba91acf37a8949ad1e01268e2be8
Ádám Berki <adamberki97@gmail.com>
2019-02-23 12:31:12 +0100

    add noContentForTheChoosentopicSnackBar when chapter 6 or 7 opens

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,9 +10,16 @@ export class AppComponent {
 
   constructor(private snackBar: MatSnackBar) { }
 
+  messageNoContent = "A választott fejezethez még nem tartozik tananyag!";
   messageFailed = "A kód nem elérhető!";
   messageSuccess = "A kód sikeresen letöltődött!";
   action = "CLOSE";
+
+  noContentForTheChoosentopicSnackBar() {
+    this.snackBar.open(this.messageNoContent, this.action, {
+      duration: 4000,
+    });
+  }
 
   basicDownloadFailedSnackBar() {
     this.snackBar.open(this.messageFailed, this.action, {
@@ -26,6 +33,14 @@ export class AppComponent {
       duration: 4000,
     });
     console.log("files successfully downloaded");
+  }
+
+  openChapter6(){
+    this.noContentForTheChoosentopicSnackBar();
+  }
+
+  openChapter7(){
+    this.noContentForTheChoosentopicSnackBar();
   }
 
 }
