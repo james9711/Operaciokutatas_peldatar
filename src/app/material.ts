693701b0b347d64d401a4c5af89bfd6f3206bc07
Ádám Berki <adamberki97@gmail.com>
2019-02-24 01:17:23 +0100

    add default stepper to chapter4 (kidolgozott feladat lépései)

--- src/app/material.ts
+++ src/app/material.ts
@@ -1,10 +1,12 @@
 import {MatButtonModule, MatCheckboxModule, MatDividerModule, MatListModule, MatSnackBarModule } from '@angular/material';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
-import { NgModule } from '@angular/core';
+import {NgModule} from '@angular/core';
+import {MatStepperModule} from '@angular/material/stepper';
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 
 @NgModule({
-  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatDividerModule, MatListModule, MatSnackBarModule ],
-  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatDividerModule, MatListModule, MatSnackBarModule ],
+  imports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatDividerModule, MatListModule, MatSnackBarModule, MatStepperModule, FormsModule, ReactiveFormsModule ],
+  exports: [MatButtonModule, MatCheckboxModule, MatToolbarModule, MatIconModule, MatDividerModule, MatListModule, MatSnackBarModule, MatStepperModule, FormsModule, ReactiveFormsModule ],
 })
 export class MaterialModule { }
